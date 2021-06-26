1ba5395bef930004806301a71c9b905ae3a8b4aa
awamej <a.wamej@gmail.com>
2021-06-26 19:21:20 +0200

    change password

--- config.py
+++ config.py
@@ -11,4 +11,4 @@ class Config:
        )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     ADMIN_USERNAME = os.environ.get("ADMIN_USERNAME", "admin")
-    ADMIN_PASSWORD = os.environ.get("ADMIN_PASSWORD", "change-me")
+    ADMIN_PASSWORD = os.environ.get("ADMIN_PASSWORD", "")
